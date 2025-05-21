0ebe4caefbfbf302154026d17e7c4750c7f594ef
Nick Young <nick.young@auckland.ac.nz>
2025-05-21 14:39:01 +1200

    Fetch GEFS 2021-2024

--- fetch_GEFS.py
+++ fetch_GEFS.py
@@ -7,17 +7,20 @@ from tqdm.contrib.concurrent import thread_map
 
 def download_GEFS(date):
     try:
-        H = Herbie(date, model="gefs_wave_reforecast", member=0, save_dir="./GEFS", verbose=False)
-        ds = H.xarray(rf":(?:HTSGW|DIRPW|PERPW):surface:(?:anl|(?:3|6|9|12|15|18|21) hour fcst)", remove_grib=True, backend_kwargs=dict(decode_timedelta=False))
-        ds = ds.sel(longitude=slice(165, 180), latitude=slice(-33,-48))
-        ds = ds.drop_vars("time").rename({"step": "time"}).assign_coords(time=ds.valid_time.values).drop_vars(["gribfile_projection", "surface", "valid_time"])
-        return ds
+        dsets = []
+        for fxx in [0, 3, 6, 9, 12, 15, 18, 21]:
+            H = Herbie(date, model="gefs", product="wave", member="p01", fxx=fxx, save_dir="./GEFS", verbose=True)
+            ds = H.xarray(rf":(?:HTSGW|DIRPW|PERPW):surface:(?:anl|(?:3|6|9|12|15|18|21) hour fcst)", remove_grib=True, backend_kwargs=dict(decode_timedelta=False))
+            ds = ds.sel(longitude=slice(165, 180), latitude=slice(-33,-48))
+            ds = ds.drop_vars("time").rename({"step": "time"}).assign_coords(time=ds.valid_time.values).drop_vars(["gribfile_projection", "surface", "valid_time"])
+            dsets.append(ds)
+        dsets = xr.concat(dsets, dim="time")
+        return dsets
     except:
         print(f"Failed to download data for {date}")
         return None
 
-# Temporal extent: 20000108-20191231
-for year in range(2009, 2020):
+for year in range(2021, 2025):
     dsets = thread_map(
         download_GEFS,
         pd.date_range(f"{year}-01-01", f"{year}-12-31", freq="D"),
